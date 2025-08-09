7c270b042cf8e67fa9013b58c3ec5c4f84d01cc9
AMIT9437 <amitsamal9437@gmail.com>
2025-08-10 00:10:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -14,7 +15,40 @@ import Blog from "./pages/Blog";
 export default function App() {
   return (
     <div style={{ fontFamily: "Arial, sans-serif" }}>
-      <Navbar />
+      {/* Bootstrap Navbar */}
+      <Navbar bg="light" expand="lg" className="shadow-sm">
+        <Container>
+          {/* Brand / Logo */}
+          <Navbar.Brand href="/">Dream 2 Success</Navbar.Brand>
+
+          {/* Hamburger menu button */}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          {/* Menu items */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/services">Services</Nav.Link>
+              <Nav.Link href="/testimonials">Testimonials</Nav.Link>
+              <Nav.Link href="/blog">Blog</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+
+            {/* Right side buttons */}
+            <div className="d-flex">
+              <Button variant="outline-primary" href="/login" className="me-2">
+                Log in
+              </Button>
+              <Button variant="primary" href="/signup">
+                Sign up
+              </Button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Main content */}
       <div style={{ minHeight: "80vh", padding: "20px" }}>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -27,6 +61,8 @@ export default function App() {
           <Route path="/blog" element={<Blog />} />
         </Routes>
       </div>
+
+      {/* Footer */}
       <Footer />
     </div>
   );
